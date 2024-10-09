75a4dd9d036b34930b77fd993b18525b5b1de77d
pruzanov <pruzanov@oicr.on.ca>
2024-10-09 11:31:13 -0400

    Harmonizing RTs with new changes

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -7,4 +7,5 @@ set -o pipefail
 cd $1
 
 #find all files, return their md5sums to std out
-find . -xtype f -iname "*.fastq.gz" -exec zcat {} | md5sum
+#find . -xtype f -iname "*.fastq.gz" -exec zcat {} | md5sum
+for f in $(find . -xtype f -name "*.fastq.gz" | sort -V);do echo $f;zcat $f | md5sum;done | paste - -
